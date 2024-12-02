3a9d3235d933a651b9aef8fc0e958bf08c29a9d3
MerajHaghverdi <merajhaghverdi2006@gmail.com>
2024-12-02 09:39:38 +0330

    changing somethings

--- First step game map/main.c
+++ First step game map/main.c
@@ -2,19 +2,13 @@
 #include <windows.h>
 #include <conio.h>
 #include <time.h>
-#include <ctime>
+
 int rows,columns;
 // function to genrating empty map
 void generate_map(char map[rows][columns]) {
-    for(int j =0; j<columns; j++){
-        map[0][j]=j;
-    }
-    for(int i =0; i<rows; i++){
-        map[i][0]=i;
-    }
-    for (int i=1; i<columns; i++){
-        for (int j=1; j<rows; j++){
-            map[rows][columns]='O'
+    for (int i=0; i<columns; i++){
+        for (int j=0; j<rows; j++){
+            map[i][j]='O';
         }
     }
 }
@@ -33,12 +27,10 @@ int main()
 {
     printf("enter rows = ");
     scanf("%d",&rows);
-    rows=rows+1;
     printf("enter columns = ");
     scanf("%d",&columns);
-    columns=columns+1;
     char map[rows][columns];
-    generate_map();
-    print_map();
+    generate_map(map);
+    print_map(map);
     return 0;
 }
